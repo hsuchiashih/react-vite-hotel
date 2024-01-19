87f54f90ca77c691aea9e3c3ce36581671fef868
hsuchiashih <hsuchiashih@gmail.com>
2024-01-20 03:10:26 +0800

    bs自定義顏色設定 & gh-page設定

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <div className="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+        <button type="button" className="btn btn-primary">Primary</button>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
